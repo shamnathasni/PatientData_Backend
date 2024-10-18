34a3506117b72363075bf5221a8823d87aec2e97
shamnathasni <shamnathasni4@gmail.com>
2024-10-17 17:24:09 -0700

    all done

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require("cors")
-require('dotenv').config(); // Ensure you require dotenv
-
+require('dotenv').config(); 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://your-frontend-url.vercel.app', 
+  }));
+  
 
-require("./config/db_config")(); // Ensure database configuration is loaded
+require("./config/db_config")(); 
 
-// Import and use routes
+
 const route = require("./routes/route");
 app.use(route);
 
