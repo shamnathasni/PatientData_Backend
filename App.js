bc4c16a06e2dc3a1d3770e88029d8ba75d19219e
shamnathasni <shamnathasni4@gmail.com>
2024-10-22 07:55:34 -0700

    deployed

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
 app.use(cors({
-    origin:"https://patient-data-frontend.vercel.app/", 
+    origin:"https://patient-data-frontend.vercel.app", 
   }));
   
 
